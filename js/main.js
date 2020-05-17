9070082884b6c9ffb8dee376e33e9326ed75b827
unknown <ahmedosamabedawy@gmail.com>
2020-05-18 01:54:25 +0200

    make All Sections Responsive

--- js/main.js
+++ js/main.js
@@ -5,10 +5,30 @@ $(function (){
     $('.portfolio ul li').on('click', function () {
         $(this).addClass('active').siblings().removeClass('active');
         if ($(this).data('class') === 'all') {
-            $('.shuffle-images .col-md').css('opacity', 1);
+            $('.shuffle-images .col-md .imghvr-zoom-out').css('opacity', 1);
         } else {
-            $('.shuffle-images .col-md').css('opacity', '.08');
+            $('.shuffle-images .col-md .imghvr-zoom-out').css('opacity', 0);
             $($(this).data('class')).parent().css('opacity', 1);
         }
+    });
+
+    // Caching The Scroll Top Element
+    var scrollButton = $("#scroll-top");
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 200)
+        {
+            scrollButton.show();
+        }
+        else
+        {
+            scrollButton.hide();
+        }
+    });
+
+    // Click On Button To Scroll Top
+
+    scrollButton.click(function () {
+        $("html,body").animate({scrollTop: 0 }, 600);
     });
 });
