553e1f5b3a6908893560db5ac09dd487d08dddf3
unknown <ahmedosamabedawy@gmail.com>
2020-05-17 03:28:32 +0200

    add Portfolio Section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function (){
+    'use strict'; 
+
+    // Featured Work Shuffle
+    $('.portfolio ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        if ($(this).data('class') === 'all') {
+            $('.shuffle-images .col-md').css('opacity', 1);
+        } else {
+            $('.shuffle-images .col-md').css('opacity', '.08');
+            $($(this).data('class')).parent().css('opacity', 1);
+        }
+    });
+});
